50d04c16dfa48f4f96e839f8e213cdcae59acb75
Guillaume Girou <porude-git@yahoo.fr>
2015-07-21 19:59:37 +0200

    Add notification option

--- js/options.js
+++ js/options.js
@@ -1,25 +1,39 @@
+NodeList.prototype.forEach = Array.prototype.forEach;
+
 var refreshTimeInput = document.getElementById('refreshTime');
 var refreshTimeSpan = document.getElementById('refreshTimeSpan');
+var refreshTimeFieldset = document.getElementById('refreshTimeFieldset');
 var statusElement = document.getElementById('status');
+
+var defaultOptions = {
+	refreshTime: 60,
+	notification: 'all'
+};
 
 // Saves options to chrome.storage.local.
 function saveOptions() {
-	var refreshTime = refreshTimeInput.value;
-
-	chrome.storage.local.set({options: {refreshTime: refreshTime}}, function () {
+	var options = {
+		refreshTime: refreshTimeInput.value,
+		notification: document.querySelector('[name=notification]:checked').value
+	};
+	refreshTimeFieldset.disabled = options.notification === 'none';
+	chrome.storage.local.set({options: options}, function () {
 		// Update status to let user know options were saved.
 		statusElement.style.visibility = "visible";
 		setTimeout(function () {
 			statusElement.style.visibility = "hidden";
-		}, 750);
+		}, 1000);
 	});
 }
 
 // Restores the preferences stored in chrome.storage.
 function restoreOptions() {
-	// Use default value color = 'red' and likesColor = true.
-	chrome.storage.local.get({options: {refreshTime: 60}}, function (objects) {
-		refreshTimeSpan.textContent = refreshTimeInput.value = objects.options.refreshTime;
+	chrome.storage.local.get({options: defaultOptions}, function (objects) {
+		var options = objects.options;
+		document.querySelector('[name=notification]:checked').checked = false;
+		document.querySelector('[name=notification][value="' + options.notification + '"]').checked = true;
+		refreshTimeFieldset.disabled = options.notification === 'none';
+		refreshTimeSpan.textContent = refreshTimeInput.value = options.refreshTime;
 	});
 }
 
@@ -28,5 +42,7 @@ function updateRefreshTimeSpan() {
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
-refreshTimeInput.addEventListener('change', saveOptions);
+document.querySelectorAll('input').forEach(function (element) {
+	element.addEventListener('change', saveOptions);
+});
 refreshTimeInput.addEventListener('input', updateRefreshTimeSpan);
