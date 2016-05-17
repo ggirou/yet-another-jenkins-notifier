019eaca323455b5908b8daffaff73ef2a7ef02ac
Guillaume Girou <porude-git@yahoo.fr>
2016-05-17 20:05:13 +0200

    Remove GA for Firefox

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ mkdir -p dist/chrome dist/firefox
 
 cp -r css fonts img js LICENSE manifest.json *.html dist/chrome
 sed -i '' '/  "applications": {/,/  },/d' dist/chrome/manifest.json
+sed -i '' 's/ data-no-firefox//' dist/chrome/*.html
 pushd dist/chrome; zip -r ../chrome.zip *; popd
 
 cp -r css fonts img js LICENSE manifest.json *.html dist/firefox
+rm dist/firefox/js/ga.js
+sed -i '' '/ data-no-firefox/d' dist/firefox/*.html
+
 pushd dist/firefox; zip -r ../firefox.xpi *; popd
