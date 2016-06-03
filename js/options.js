b36e068dd5fdf9361bf443ef67322b72afd11788
Guillaume Girou <porude-git@yahoo.fr>
2016-06-03 10:37:52 +0200

    Encapsulation

--- js/options.js
+++ js/options.js
@@ -15,51 +15,56 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-NodeList.prototype.forEach = Array.prototype.forEach;
 
-var refreshTimeInput = document.getElementById('refreshTime');
-var refreshTimeSpan = document.getElementById('refreshTimeSpan');
-var refreshTimeFieldset = document.getElementById('refreshTimeFieldset');
-var statusElement = document.getElementById('status');
+(function () {
+  "use strict";
 
-var defaultOptions = {
-  refreshTime: 60,
-  notification: 'all'
-};
+  NodeList.prototype.forEach = Array.prototype.forEach;
 
-// Saves options to chrome.storage.local.
-function saveOptions() {
-  var options = {
-    refreshTime: refreshTimeInput.value,
-    notification: document.querySelector('[name=notification]:checked').value
+  var refreshTimeInput = document.getElementById('refreshTime');
+  var refreshTimeSpan = document.getElementById('refreshTimeSpan');
+  var refreshTimeFieldset = document.getElementById('refreshTimeFieldset');
+  var statusElement = document.getElementById('status');
+
+  var defaultOptions = {
+    refreshTime: 60,
+    notification: 'all'
   };
-  refreshTimeFieldset.disabled = options.notification === 'none';
-  chrome.storage.local.set({options: options}, function () {
-    // Update status to let user know options were saved.
-    statusElement.style.visibility = "visible";
-    setTimeout(function () {
-      statusElement.style.visibility = "hidden";
-    }, 1000);
-  });
-}
 
-// Restores the preferences stored in chrome.storage.
-function restoreOptions() {
-  chrome.storage.local.get({options: defaultOptions}, function (objects) {
-    var options = objects.options;
-    document.querySelector('[name=notification]:checked').checked = false;
-    document.querySelector('[name=notification][value="' + options.notification + '"]').checked = true;
+  // Saves options to chrome.storage.local.
+  function saveOptions() {
+    var options = {
+      refreshTime: refreshTimeInput.value,
+      notification: document.querySelector('[name=notification]:checked').value
+    };
     refreshTimeFieldset.disabled = options.notification === 'none';
-    refreshTimeSpan.textContent = refreshTimeInput.value = options.refreshTime;
+    chrome.storage.local.set({options: options}, function () {
+      // Update status to let user know options were saved.
+      statusElement.style.visibility = "visible";
+      setTimeout(function () {
+        statusElement.style.visibility = "hidden";
+      }, 1000);
+    });
+  }
+
+  // Restores the preferences stored in chrome.storage.
+  function restoreOptions() {
+    chrome.storage.local.get({options: defaultOptions}, function (objects) {
+      var options = objects.options;
+      document.querySelector('[name=notification]:checked').checked = false;
+      document.querySelector('[name=notification][value="' + options.notification + '"]').checked = true;
+      refreshTimeFieldset.disabled = options.notification === 'none';
+      refreshTimeSpan.textContent = refreshTimeInput.value = options.refreshTime;
+    });
+  }
+
+  function updateRefreshTimeSpan() {
+    refreshTimeSpan.textContent = refreshTimeInput.value;
+  }
+
+  document.addEventListener('DOMContentLoaded', restoreOptions);
+  document.querySelectorAll('input').forEach(function (element) {
+    element.addEventListener('change', saveOptions);
   });
-}
-
-function updateRefreshTimeSpan() {
-  refreshTimeSpan.textContent = refreshTimeInput.value;
-}
-
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelectorAll('input').forEach(function (element) {
-  element.addEventListener('change', saveOptions);
-});
-refreshTimeInput.addEventListener('input', updateRefreshTimeSpan);
+  refreshTimeInput.addEventListener('input', updateRefreshTimeSpan);
+})();
