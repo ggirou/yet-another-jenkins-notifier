8b123a02531ffa7b96e861856620b893f86fd8b4
Guillaume Girou <porude-git@yahoo.fr>
2016-07-11 21:27:00 +0200

    BSD and Linux compatible build script

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@ rm -rf dist
 mkdir -p dist/chrome dist/firefox
 
 cp -r css fonts img js LICENSE manifest.json *.html dist/chrome
-sed -i '' '/  "applications": {/,/  },/d' dist/chrome/manifest.json
-sed -i '' 's/ data-no-firefox//' dist/chrome/*.html
-pushd dist/chrome; zip -r ../chrome.zip *; popd
+sed -i.bak '/  "applications": {/,/  },/d' dist/chrome/manifest.json
+sed -i.bak 's/ data-no-firefox//' dist/chrome/*.html
+pushd dist/chrome; rm *.bak; zip -r ../chrome.zip *; popd
 
 cp -r css fonts img js LICENSE manifest.json *.html dist/firefox
 rm dist/firefox/js/ga.js
-sed -i '' '/"author"/d' dist/firefox/manifest.json
-sed -i '' '/"minimum_chrome_version"/d' dist/firefox/manifest.json
-sed -i '' '/"content_security_policy"/d' dist/firefox/manifest.json
-sed -i '' '/"browser_style"/d' dist/firefox/manifest.json
-sed -i '' 's/, "js\/ga.js"//' dist/firefox/manifest.json
-sed -i '' '/ data-no-firefox/d' dist/firefox/*.html
+sed -i.bak '/"author"/d' dist/firefox/manifest.json
+sed -i.bak '/"minimum_chrome_version"/d' dist/firefox/manifest.json
+sed -i.bak '/"content_security_policy"/d' dist/firefox/manifest.json
+sed -i.bak '/"browser_style"/d' dist/firefox/manifest.json
+sed -i.bak 's/, "js\/ga.js"//' dist/firefox/manifest.json
+sed -i.bak '/ data-no-firefox/d' dist/firefox/*.html
 
-pushd dist/firefox; zip -r ../firefox.xpi *; popd
+pushd dist/firefox; rm *.bak; zip -r ../firefox.xpi *; popd
